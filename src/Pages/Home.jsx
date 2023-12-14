071fc57d053fab6695cdd41c694405d289182bb6
RobiAdamescu <robiadamescu@gmail.com>
2023-12-14 21:15:14 +0200

    Modified Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,12 +5,22 @@ import { Button } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import background from '../imgs/poza.jpg';
 import ScrollControlButtons from '../Components/ScrollControlButtons';
 import videoclip from '../imgs/video.mp4';
+import background from '../imgs/poza.jpg';
 
 function Home() {
   const { darkMode } = useDarkMode();
+
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: '4rem',
+    backgroundColor: darkMode ? '#333' : '#f5f5f5',
+    color: darkMode ? '#fff' : '#000',
+  };
 
   return (
     <>
@@ -18,45 +28,48 @@ function Home() {
         <Navbar />
       </div>
 
-     
-      
-
-      <div style={{ backgroundColor: darkMode ? '#333' : '#f5f5f5', backgroundImage: `url(${background})`, backgroundSize: 'cover', padding: '  4rem', textAlign: 'center' }}>
-        <h1 style={{ color: darkMode ? '#fff' : '#fff', fontSize: '24px', marginBottom: '10px' }}>
-          Welcome to CarCharms - Your Source for Stylish Keychain Accessories!
-        </h1>
-        <p style={{ color: darkMode ? '#fff' : '#fff', fontSize: '16px', marginBottom: '20px' }}>
-          At CarCharms, we believe that even the smallest details can make a big difference. Elevate your driving experience
-          with our premium collection of car keychain accessories designed to add style and personality to your keys.
-        </p>
-        <h2 style={{ color: darkMode ? '#fff' : '#fff', fontSize: '18px', marginBottom: '10px' }}>
-          What We Offer:
-        </h2>
-        <p style={{ color: darkMode ? '#fff' : '#fff', fontSize: '14px', marginBottom: '20px' }}>
-          Explore our diverse range of keychain accessories, crafted with precision and passion. Whether you're a car enthusiast
-          or looking for a thoughtful gift, our collection has something for everyone. From sleek and modern designs to
-          vintage-inspired charms, we have the perfect accessory to match your style.
-        </p>
+      <div style={containerStyle}>
+        <div style={{ flex: '1', maxWidth: '50%' }}>
+          <h1 style={{ fontSize: '24px', marginBottom: '10px' }}>
+            Welcome to CarCharms - Your Source for Stylish Keychain Accessories!
+          </h1>
+          <p style={{ fontSize: '16px', marginBottom: '20px' }}>
+            At CarCharms, we believe that even the smallest details can make a big difference. Elevate your driving experience
+            with our premium collection of car keychain accessories designed to add style and personality to your keys.
+          </p>
+          <h2 style={{ fontSize: '18px', marginBottom: '10px' }}>
+            What We Offer:
+          </h2>
+          <p style={{ fontSize: '14px', marginBottom: '20px' }}>
+            Explore our diverse range of keychain accessories, crafted with precision and passion. Whether you're a car enthusiast
+            or looking for a thoughtful gift, our collection has something for everyone. From sleek and modern designs to
+            vintage-inspired charms, we have the perfect accessory to match your style.
+          </p>
+        </div>
+        <div style={{ flex: '1', maxWidth: '50%', marginLeft: '20px' }}>
+          <img src={background} alt="CarCharms" style={{ width: '100%', borderRadius: '10px' }} />
+        </div>
       </div>
-      <div style={{ position: 'relative', overflow: 'hidden' }}>
+
+      <div style={{ position: 'relative', overflow: 'hidden', margin: '20px auto', maxWidth: '70%', backgroundColor: darkMode ? '#333' : '#f5f5f5' }}>
         <video
           autoPlay
           loop
           muted
           controls
           style={{
-            // position: 'absolute',
             width: '100%',
             height: '50%',
             objectFit: 'cover',
-            zIndex: '5',
-            borderRadius: '10px'
+            borderRadius: '10px',
           }}
         >
           <source src={videoclip} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
+
+
       <ScrollControlButtons
         style={{
           position: 'fixed',
@@ -87,7 +100,7 @@ function Home() {
         <Button component="a" href="https://twitter.com/" target="_blank" rel="noopener noreferrer" color="primary" variant="text" style={{ margin: '0 10px' }}>
           <TwitterIcon />
         </Button>
-        <p style={{ color: darkMode ? '#fff' : 'rgba(0, 0, 0, 0.87)', marginTop: '10px', fontSize: '12px' }}>
+        <p style={{ color: darkMode ? '#fff' : 'rgba(0, 0, 0, 0.87)', marginTop: '10px', fontSize: '14px' }}>
           &copy; 2023 CarCharms. All rights reserved.
         </p>
       </div>
